e0c2de7fa9447f5c9bdbbb5562c0ba4ad58f5ed9
atishkumar123 <atishkumar12548@gmail.com>
2020-05-07 03:01:24 +0530

    firstcommit

---
+++ banckaccount/src/banckaccount/main.java
@@ -0,0 +1,19 @@
+package banckaccount;
+
+public class main {
+
+	public static void main(String[] args) {
+		customerDetails atish=new customerDetails();
+		customerDetails rupa=new customerDetails();
+	    customerDetails jyoti=new customerDetails();
+		customerDetails puja=new customerDetails();
+		customerDetails rajendra=new customerDetails();
+		
+		
+          
+          rupa.account();
+         // rupa.login();
+          
+	}
+
+}
